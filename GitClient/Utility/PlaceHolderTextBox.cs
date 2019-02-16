873b03f19515f18d7b4c05685cc2722b6f69c780
HariKChavan <hari.net.trainer@gmail.com>
2019-02-17 01:36:55 +0530

    placefoleder

---
+++ GitClient/Utility/PlaceHolderTextBox.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace System.Windows.Forms
+{
+    public class PlaceHolderTextBox : TextBox
+    {
+        bool isPasswordText = false;
+        bool isPlaceHolder = true;
+        string _placeHolderText;
+        public string PlaceHolderText
+        {
+            get { return _placeHolderText; }
+            set
+            {
+                _placeHolderText = value;
+                if (!base.PasswordChar.Equals(char.MinValue))
+                {
+                    isPasswordText = true;
+                }
+                setPlaceholder();
+            }
+        }
+
+        //when the control loses focus, the placeholder is shown
+        private void setPlaceholder()
+        {
+            if (string.IsNullOrEmpty(base.Text))
+            {
+                base.PasswordChar = char.MinValue;
+                base.Text = PlaceHolderText;
+                this.ForeColor = Color.Gray;
+                this.Font = new Font(this.Font, FontStyle.Italic);
+                isPlaceHolder = true;
+            }
+        }
+
+        //when the control is focused, the placeholder is removed
+        private void removePlaceHolder()
+        {
+
+            if (isPlaceHolder)
+            {
+                base.PasswordChar = isPasswordText ? '*' : char.MinValue;
+                base.Text = "";
+                this.ForeColor = System.Drawing.SystemColors.WindowText;
+                this.Font = new Font(this.Font, FontStyle.Regular);
+                isPlaceHolder = false;
+            }
+        }
+        public PlaceHolderTextBox()
+        {
+            GotFocus += removePlaceHolder;
+            LostFocus += setPlaceholder;
+            if (!base.PasswordChar.Equals(char.MinValue))
+            {
+                isPasswordText = true;
+            }
+        }
+
+        private void setPlaceholder(object sender, EventArgs e)
+        {
+            setPlaceholder();
+        }
+
+        private void removePlaceHolder(object sender, EventArgs e)
+        {
+            removePlaceHolder();
+        }
+    }
+}
